f5a53f9a0aaf2ffd31ea296d3af073811fb33d56
Denis Iogansen <pofigizm@gmail.com>
2018-10-21 13:17:30 +0300

    add name

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 
+const devEnv = process && process.env && process.env.NODE_ENV !== 'production'
 const configureStore = (initialState, rootReducer, dynamicMiddlewares) => {
   const middlewares = applyMiddleware(dynamicMiddlewares)
   // eslint-disable-next-line no-underscore-dangle
   const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  const composeEnhancers = process.env.NODE_ENV !== 'production' && devTools ? devTools : compose
+  const composeEnhancers = devEnv && devTools ? devTools({ name: 'redux-dynamic' }) : compose
   const enhancers = composeEnhancers(middlewares)
   const store = createStore(rootReducer, initialState, enhancers)
 
