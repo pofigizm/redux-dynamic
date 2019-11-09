ed4591a2318120d3c4f80da7e85547000de2e6df
Darya Saenko <dssaenko@avito.ru>
2019-11-09 23:45:32 +0300

    Make create instance flat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createInstance } from './create-instance'
+import { createInstanceFlat } from './create-instance-flat'
 import { prepareActions } from './prepare-actions'
 
 export {
   createInstance,
+  createInstanceFlat,
   prepareActions,
 }
